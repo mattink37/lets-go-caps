6486ea956632c393a42a5c5224fd14e2f329e9b4
mattink37 <mattinkeles37@gmail.com>
2020-01-31 23:14:51 -0500

    +minkeles Creation of GameLogic class

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,11 +2,32 @@
 
 namespace Snake
 {
+  class GameLogic
+  {
+    public GameLogic()
+    {
+      while (true)
+      {
+        input();
+        update();
+      }
+    }
+
+    void update()
+    {
+      Console.Clear();
+    }
+
+    void input()
+    {
+
+    }
+  }
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      GameLogic gameLogic = new GameLogic();
     }
   }
 }
