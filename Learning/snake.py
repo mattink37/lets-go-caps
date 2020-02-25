5bd3d1ba2e0c1c5b1193ce2c18764c6b3a849649
1027175 <Matt.Inkeles@jda.com>
2020-02-25 15:53:20 -0500

    beginning leaderboard

--- Learning/snake.py
+++ Learning/snake.py
@@ -197,7 +197,36 @@ class GameLogic:
             self._update()
 
 
+class LeaderBoard:
+
+    def __init__(self):
+        file = open("C:\git\hmt\Learning\leaderboard.txt", "r")
+        self.leaderboard = []
+        i = 0
+        while i < 5:
+            chunk = file.readline()
+            if (chunk == ''):
+                break
+            LeaderBoard.insert(0, chunk.splitlines())
+            i += 1
+        file.close()
+        self.leaderboard.reverse()
+
+    @staticmethod
+    def addToLeaderboard(self, name, newScore):
+        i = 0
+        for score in self.leaderboard:
+            if newScore > score:
+                self.leaderboard.remove(score)
+                self.leaderboard.insert(i, newScore)
+            i += 1
+        file = open("C:\git\hmt\Learning\leaderboard.txt", "w")
+        # add name and score
+        file.close()
+
+
 def main():
+    l = LeaderBoard()
     game = GameLogic(500, 500)
 
 
